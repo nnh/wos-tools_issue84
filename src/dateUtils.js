1ae61f4152d218efd0829e24bf89f3fe6e8198df
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2023-09-20 12:22:19 +0900

    first commit.

---
+++ src/dateUtils.js
@@ -0,0 +1,16 @@
+class DateUtils {
+  constructor() {}
+  formatDate(date) {
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    return `${year}/${month}/${day}`;
+  }
+  formatDateTime(date) {
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    const seconds = ('0' + date.getSeconds()).slice(-2);
+    const ymd = this.formatDate(date);
+    return `${ymd} ${hours}:${minutes}:${seconds}`;
+  }
+}
